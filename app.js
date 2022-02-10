7827a0c8d2fb7ad017b4972e42aaa601d5304aab
rebreather <yds04116@gmail.com>
2022-02-10 15:44:01 +0900

    # 4.1

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+
+function loginBtnSubmit(info) {
+    info.preventDefault();
+    //const username = loginInput.value;
+    console.log(info);
+}
+
+loginForm.addEventListener("submit", loginBtnSubmit);
