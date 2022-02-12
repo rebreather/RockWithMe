d9d81a35842260e6bc25ba7e0b62e3a1486925dd
rebreather <yds04116@gmail.com>
2022-02-12 22:05:30 +0900

    220212 #4.4 폼에 이름 받고 제출 후 환영 인사 보이기

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 ﻿const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function loginBtnSubmit(info) {
   info.preventDefault();
-  //const username = loginInput.value;
-  console.log(info);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.log(event);
-  alert("click!"); //기본동작을 막지만 확인을 누르면 동작이 실행 됨
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME); //loginForm에 hidden이라는 클래스를 추가한다.
+  greeting.innerText = `Hello ${username}! Have a good day :)`; //greeting.innerText = "hello " + username;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", loginBtnSubmit);
-link.addEventListener("click", handleLinkClick)
-
-//새로고침은 form submit의 기본 동작이다.
-//preventDefault() 함수는 어떤 event의 기본 행동이든지 발생되지 않도록 막는다.
-//링크의 기본 동작은 다른 페이지로 넘어가는 것이다.
