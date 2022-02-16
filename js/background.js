1842e46b332048f552fbf3c0a25ac1120f8a1d6f
rebreather <yds04116@gmail.com>
2022-02-17 00:07:18 +0900

    #6.0 ~ 6.2 인용과 배경화면 랜덤 출력

---
+++ js/background.js
@@ -0,0 +1,12 @@
+﻿const images = [
+    "0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg",
+    "5.jpg", "6.jpg", "7.jpg", "8.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random()*(images.length))];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+//appendChild())는 body에 html을 추가함
+document.body.appendChild(bgImage);
---
+++ js/quetoes.js
@@ -0,0 +1,32 @@
+﻿const quotes = [
+    {
+        quote: "야 밥을 맛있게 먹는 방법이 어딨냐 그냥 먹으면 맛있는 거지"
+    },
+    {
+        quote: "음. Tasty."
+    },
+    {
+        quote: "나는 노력을 타고났다"
+    },
+    {
+        quote: "이거 멤버들에게 비밀이지만 난 너희 없음 음악 안 해"
+    },
+    {
+        quote: "서로만 믿고 가자"
+    },
+    {
+        quote: "행복의 정의는 없어요"
+    },
+    {
+        quote: "미적 기준을 다른 사람에게 맞추려 하지마요"
+    },
+    {
+        quote: "하기싫은 걸 해야 하고싶은 것도 한다"
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+
+const todayQuote = quotes[Math.floor(Math.random()*(quotes.length))];
+
+quote.innerText = todayQuote.quote;
