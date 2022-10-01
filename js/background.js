6144e66813c31cb6373c1dd329cd5eb1a2e59a0f
rebreather <yds04116@gmail.com>
2022-10-01 22:27:00 +0900

    버전 업, 기타 수정

--- js/background.js
+++ js/background.js
@@ -11,7 +11,8 @@ const images = [
     "21.png", "22.png", "23.png", "24.png", "25.png", "26.png", "27.png", "28.png", "29.png", "30.png",
     "31.png", "32.png", "33.png", "34.png", "35.png", "36.png", "37.png", "38.png", "39.png", "40.png",
     "41.png", "42.png", "43.png", "44.png", "45.png", "46.png", "47.png", "48.png", "49.png", "50.png",
-    "51.png", "52.png", "53.png", "54.png", "55.png", "56.png", "57.png", "58.png", "59.png", "60.png"
+    "51.png", "52.png", "53.png", "54.png", "55.png", "56.png", "57.png", "58.png", "59.png", "60.png",
+    "61.png", "62.png", "63.png", "64.png", "65.png", "66.png", "67.png", "68.png"
 ];
 
 function paintImage(imgNumber) {
