b6198a327584f331d409d34745691c6840596f23
rebreather <yds04116@gmail.com>
2024-07-19 18:56:52 +0900

    chore: tailwind, postcss, autoprefixer 설치

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,cjs,mjs,}'],
+  content: [],
+  theme: {
+    extend: {},
+    // screens: {
+    //   lg: '1024px',
+    //   xl: '1280px'
+    // }
+  },
+  plugins: [],
+}
+
